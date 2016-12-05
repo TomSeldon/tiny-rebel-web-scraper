9c7ec6d5e86725ed3dd540cd2a3c6fe0b31f121c
tomseldon <tom@tomseldon.co.uk>
2016-12-05 11:16:58 +0000

    [FORMAT] Fix linting errors in unit tests

--- test/unit/api/api.spec.ts
+++ test/unit/api/api.spec.ts
@@ -13,7 +13,7 @@ chai.use(sinonChai);
 describe('API', () => {
     let api: API;
     let requestSpy: sinon.SinonStub;
-    let mockDrinks: Array<Drink>;
+    let mockDrinks: Drink[];
 
     beforeEach(() => {
         mockDrinks = [];
@@ -38,17 +38,17 @@ describe('API', () => {
     });
 
     describe('getting all drinks', () => {
-        it('should not allow drinks to be requested when a bar location has not been specified', done => {
+        it('should not allow drinks to be requested when a bar location has not been specified', (done) => {
             api.getAllDrinks('')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('No bar location specified');
                     done();
                 });
         });
 
-        it('should not allow drinks to be requested for unsupported bar locations', done => {
+        it('should not allow drinks to be requested for unsupported bar locations', (done) => {
             api.getAllDrinks('an unsupported bar')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unknown bar location specified');
                     done();
                 });
@@ -57,22 +57,20 @@ describe('API', () => {
         it('should make a GET request to the correct URL', () => {
             api.getAllDrinks('cardiff');
 
-            expect(requestSpy).to.have.been.calledWith('http://www.tinyrebel.co.uk/bars/cardiff/beer-board/')
+            expect(requestSpy).to.have.been.calledWith('http://www.tinyrebel.co.uk/bars/cardiff/beer-board/');
         });
 
-        it('should reject when the HTTP call errors', done => {
-            const error = new Error('Some HTTP error');
-
-            requestSpy.yields(error);
+        it('should reject when the HTTP call errors', (done) => {
+            requestSpy.yields(new Error('Some HTTP error'));
 
             api.getAllDrinks('cardiff')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unable to load beer board');
                     done();
                 });
         });
 
-        it('should reject when the HTTP request results in a server error', done => {
+        it('should reject when the HTTP request results in a server error', (done) => {
             const response = {
                 statusCode: 500
             };
@@ -80,13 +78,13 @@ describe('API', () => {
             requestSpy.yields(null, response);
 
             api.getAllDrinks('cardiff')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unable to load beer board');
                     done();
                 });
         });
 
-        it('should reject when the HTTP request results in a 404 not found', done => {
+        it('should reject when the HTTP request results in a 404 not found', (done) => {
             const response = {
                 statusCode: 404
             };
@@ -94,27 +92,27 @@ describe('API', () => {
             requestSpy.yields(null, response);
 
             api.getAllDrinks('cardiff')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unable to load beer board');
                     done();
                 });
         });
 
-        it('should resolve with an array of drinks', done => {
+        it('should resolve with an array of drinks', (done) => {
             mockDrinks = [
                 {
+                    abv: '4.2%',
+                    available: true,
+                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
+                    cask: true,
+                    currency: 'GBP',
+                    formattedPrice: '£3.30',
+                    keg: false,
                     name: 'You Snows It',
                     price: 3.3,
-                    formattedPrice: '£3.30',
-                    currency: 'GBP',
-                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
-                    style: 'Pale Ale',
-                    abv: '4.2%',
-                    vegan: false,
                     quantity: 'pint',
-                    available: true,
-                    keg: false,
-                    cask: true
+                    style: 'Pale Ale',
+                    vegan: false
                 }
             ];
 
@@ -122,37 +120,39 @@ describe('API', () => {
                 statusCode: 200
             };
 
-            // The parsed drinks are mocked, so we just need to pass in a string for the body
-            // but the actual content doesn't matter here
+            /*
+              The parsed drinks are mocked, so we just need to pass in a string for the body
+              but the actual content doesn't matter here
+            */
             const body = '';
 
             requestSpy.yields(null, response, body);
 
             api.getAllDrinks('cardiff')
-                .then((drinks: Array<Drink>) => {
+                .then((drinks: Drink[]) => {
                     expect(drinks.length).to.equal(1);
                     done();
                 })
-                .catch(error => {
+                .catch((error: Error) => {
                     if (error) {
                         done(error);
                     }
-                })
+                });
         });
     });
 
     describe('getting all keg drinks', () => {
-        it('should not allow drinks to be requested when a bar location has not been specified', done => {
+        it('should not allow drinks to be requested when a bar location has not been specified', (done) => {
             api.getAllKegDrinks('')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('No bar location specified');
                     done();
                 });
         });
 
-        it('should not allow drinks to be requested for unsupported bar locations', done => {
+        it('should not allow drinks to be requested for unsupported bar locations', (done) => {
             api.getAllKegDrinks('an unsupported bar')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unknown bar location specified');
                     done();
                 });
@@ -161,22 +161,20 @@ describe('API', () => {
         it('should make a GET request to the correct URL', () => {
             api.getAllKegDrinks('cardiff');
 
-            expect(requestSpy).to.have.been.calledWith('http://www.tinyrebel.co.uk/bars/cardiff/beer-board/')
+            expect(requestSpy).to.have.been.calledWith('http://www.tinyrebel.co.uk/bars/cardiff/beer-board/');
         });
 
-        it('should reject when the HTTP call errors', done => {
-            const error = new Error('Some HTTP error');
-
-            requestSpy.yields(error);
+        it('should reject when the HTTP call errors', (done) => {
+            requestSpy.yields(new Error('Some HTTP error'));
 
             api.getAllKegDrinks('cardiff')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unable to load beer board');
                     done();
                 });
         });
 
-        it('should reject when the HTTP request results in a server error', done => {
+        it('should reject when the HTTP request results in a server error', (done) => {
             const response = {
                 statusCode: 500
             };
@@ -184,13 +182,13 @@ describe('API', () => {
             requestSpy.yields(null, response);
 
             api.getAllKegDrinks('cardiff')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unable to load beer board');
                     done();
                 });
         });
 
-        it('should reject when the HTTP request results in a 404 not found', done => {
+        it('should reject when the HTTP request results in a 404 not found', (done) => {
             const response = {
                 statusCode: 404
             };
@@ -198,41 +196,41 @@ describe('API', () => {
             requestSpy.yields(null, response);
 
             api.getAllKegDrinks('cardiff')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unable to load beer board');
                     done();
                 });
         });
 
-        it('should resolve with an array of only the keg drinks', done => {
+        it('should resolve with an array of only the keg drinks', (done) => {
             mockDrinks = [
                 {
+                    abv: '4.2%',
+                    available: true,
+                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
+                    cask: true,
+                    currency: 'GBP',
+                    formattedPrice: '£3.30',
+                    keg: false,
                     name: 'You Snows It',
                     price: 3.3,
-                    formattedPrice: '£3.30',
-                    currency: 'GBP',
-                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
-                    style: 'Pale Ale',
-                    abv: '4.2%',
-                    vegan: false,
                     quantity: 'pint',
-                    available: true,
-                    keg: false,
-                    cask: true
+                    style: 'Pale Ale',
+                    vegan: false
                 },
                 {
+                    abv: '4.2%',
+                    available: true,
+                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
+                    cask: false,
+                    currency: 'GBP',
+                    formattedPrice: '£3.70',
+                    keg: true,
                     name: 'Urban Pils',
                     price: 3.7,
-                    formattedPrice: '£3.70',
-                    currency: 'GBP',
-                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
-                    style: 'Pilsner',
-                    abv: '4.2%',
-                    vegan: false,
                     quantity: 'pint',
-                    available: true,
-                    keg: true,
-                    cask: false
+                    style: 'Pilsner',
+                    vegan: false
                 }
             ];
 
@@ -240,38 +238,40 @@ describe('API', () => {
                 statusCode: 200
             };
 
-            // The parsed drinks are mocked, so we just need to pass in a string for the body
-            // but the actual content doesn't matter here
+            /*
+              The parsed drinks are mocked, so we just need to pass in a string for the body
+              but the actual content doesn't matter here
+            */
             const body = '';
 
             requestSpy.yields(null, response, body);
 
             api.getAllKegDrinks('cardiff')
-                .then((drinks: Array<Drink>) => {
+                .then((drinks: Drink[]) => {
                     expect(drinks.length).to.equal(1);
                     expect(drinks[0]).to.deep.equal(mockDrinks[1]);
                     done();
                 })
-                .catch(error => {
+                .catch((error: Error) => {
                     if (error) {
                         done(error);
                     }
-                })
+                });
         });
     });
 
     describe('getting all cask drinks', () => {
-        it('should not allow drinks to be requested when a bar location has not been specified', done => {
+        it('should not allow drinks to be requested when a bar location has not been specified', (done) => {
             api.getAllCaskDrinks('')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('No bar location specified');
                     done();
                 });
         });
 
-        it('should not allow drinks to be requested for unsupported bar locations', done => {
+        it('should not allow drinks to be requested for unsupported bar locations', (done) => {
             api.getAllCaskDrinks('an unsupported bar')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unknown bar location specified');
                     done();
                 });
@@ -280,22 +280,20 @@ describe('API', () => {
         it('should make a GET request to the correct URL', () => {
             api.getAllCaskDrinks('cardiff');
 
-            expect(requestSpy).to.have.been.calledWith('http://www.tinyrebel.co.uk/bars/cardiff/beer-board/')
+            expect(requestSpy).to.have.been.calledWith('http://www.tinyrebel.co.uk/bars/cardiff/beer-board/');
         });
 
-        it('should reject when the HTTP call errors', done => {
-            const error = new Error('Some HTTP error');
-
-            requestSpy.yields(error);
+        it('should reject when the HTTP call errors', (done) => {
+            requestSpy.yields(new Error('Some HTTP error'));
 
             api.getAllCaskDrinks('cardiff')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unable to load beer board');
                     done();
                 });
         });
 
-        it('should reject when the HTTP request results in a server error', done => {
+        it('should reject when the HTTP request results in a server error', (done) => {
             const response = {
                 statusCode: 500
             };
@@ -303,13 +301,13 @@ describe('API', () => {
             requestSpy.yields(null, response);
 
             api.getAllCaskDrinks('cardiff')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unable to load beer board');
                     done();
                 });
         });
 
-        it('should reject when the HTTP request results in a 404 not found', done => {
+        it('should reject when the HTTP request results in a 404 not found', (done) => {
             const response = {
                 statusCode: 404
             };
@@ -317,41 +315,41 @@ describe('API', () => {
             requestSpy.yields(null, response);
 
             api.getAllCaskDrinks('cardiff')
-                .catch(error => {
+                .catch((error: Error) => {
                     expect(error.message).to.equal('Unable to load beer board');
                     done();
                 });
         });
 
-        it('should resolve with an array of only the cask drinks', done => {
+        it('should resolve with an array of only the cask drinks', (done) => {
             mockDrinks = [
                 {
+                    abv: '4.2%',
+                    available: true,
+                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
+                    cask: true,
+                    currency: 'GBP',
+                    formattedPrice: '£3.30',
+                    keg: false,
                     name: 'You Snows It',
                     price: 3.3,
-                    formattedPrice: '£3.30',
-                    currency: 'GBP',
-                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
-                    style: 'Pale Ale',
-                    abv: '4.2%',
-                    vegan: false,
                     quantity: 'pint',
-                    available: true,
-                    keg: false,
-                    cask: true
+                    style: 'Pale Ale',
+                    vegan: false
                 },
                 {
+                    abv: '4.2%',
+                    available: true,
+                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
+                    cask: false,
+                    currency: 'GBP',
+                    formattedPrice: '£3.70',
+                    keg: true,
                     name: 'Urban Pils',
                     price: 3.7,
-                    formattedPrice: '£3.70',
-                    currency: 'GBP',
-                    brewery: 'Tiny Rebel Brewing Co. (Newport, South Wales)',
-                    style: 'Pilsner',
-                    abv: '4.2%',
-                    vegan: false,
                     quantity: 'pint',
-                    available: true,
-                    keg: true,
-                    cask: false
+                    style: 'Pilsner',
+                    vegan: false
                 }
             ];
 
@@ -359,23 +357,25 @@ describe('API', () => {
                 statusCode: 200
             };
 
-            // The parsed drinks are mocked, so we just need to pass in a string for the body
-            // but the actual content doesn't matter here
+            /*
+              The parsed drinks are mocked, so we just need to pass in a string for the body
+              but the actual content doesn't matter here
+            */
             const body = '';
 
             requestSpy.yields(null, response, body);
 
             api.getAllCaskDrinks('cardiff')
-                .then((drinks: Array<Drink>) => {
+                .then((drinks: Drink[]) => {
                     expect(drinks.length).to.equal(1);
                     expect(drinks[0]).to.deep.equal(mockDrinks[0]);
                     done();
                 })
-                .catch(error => {
+                .catch((error: Error) => {
                     if (error) {
                         done(error);
                     }
-                })
+                });
         });
     });
 });
